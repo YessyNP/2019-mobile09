f77afe172eaed4ef22cfe085db445b44c81f7406
YessyNP <yessynindi@gmail.com>
2019-10-21 18:30:51 +0700

    Recycler view gambar dan text serta Scroll

---
+++ RecyclerviewGambardanTextSertaScroll/app/src/main/java/com/yessy/recyclerviewgambardantextsertascroll/models/SuperHero.java
@@ -0,0 +1,40 @@
+package com.yessy.recyclerviewgambardantextsertascroll.models;
+
+public class SuperHero {
+        public String heroName;
+        public String heroText;
+        public int heroImage;
+
+        public SuperHero(String heroName, String heroText, int heroImage) {
+            this.heroName = heroName;
+            this.heroText = heroText;
+            this.heroImage = heroImage;
+        }
+
+        public String getHeroName() {
+            return heroName;
+        }
+
+
+        public void setHeroName(String heroName) {
+            this.heroName = heroName;
+
+        }
+
+
+        public String getHeroText() {
+            return heroText;
+        }
+        public void setHeroText(String heroText) {
+            this.heroText = heroText;
+        }
+
+        public int getHeroImage() {
+            return heroImage;
+        }
+
+
+        public void setHeroImage(int heroImage) {
+            this.heroImage = heroImage;
+        }
+    }
